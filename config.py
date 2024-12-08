2670504dfe4121b162bb53aca73ebbb4677fb708
Balaji G <100072354+Balaji-G-programmer@users.noreply.github.com>
2024-12-08 07:07:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 
 #Bot token @Botfather
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7836416496:AAEJyWBIaocQQNVrN9R-kaQqSPcVx2TUfQk")
 
 #Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", ""))
+API_ID = int(os.environ.get("API_ID", "11112771"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "4606a7fdabe4eb7f6c0b1ce77f6372ce")
 
 #Database 
-DB_URI = os.environ.get("DB_URI", "")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://balajig9898:balajig2003@save-restricted-bot.fkdsd.mongodb.net/?retryWrites=true&w=majority&appName=Save-Restricted-Bot")
